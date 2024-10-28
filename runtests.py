ef1d80004370733ee4d4f89d55f088f8bb56db2d
Lee Campbell <leecam@google.com>
2024-10-27 18:49:38 -0700

    Adding basic test cases

---
+++ runtests.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+import dcql
+
+def get_test_cases(path):
+    return [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.startswith('testcase')]
+
+def load_json(path):
+    with open(path) as f:
+        return json.load(f)
+
+def main():
+    test_cases = get_test_cases(os.getcwd())
+    test_cases.sort()
+    print(test_cases)
+    credential_store = load_json('credentials.json')
+    for test_path in test_cases:
+        test = load_json(test_path)
+        print('Running test: {}'.format(test['name']))
+
+        matched_credentials = dcql.dcql_query(test['dcql_query'], credential_store['credentials'])
+        expected_result = test['expected_result']['matched_credentials'] 
+        if expected_result == matched_credentials:
+            print('PASS')
+        else:
+            print('FAIL')
+        #print('Matched : {}'.format(json.dumps(matched_credentials)))
+        #print('Expected: {}'.format(expected_result))
+
+if __name__ == '__main__':
+    main()
